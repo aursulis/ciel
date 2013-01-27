ce6e72f85d1de583a1f0dfe16ebac503f140b495
Antanas Uršulis <antanas@cantab.net>
2013-01-27 21:55:06 +0000

    Move to new directory

--- src/c/libcielshm/test/clnt.c
+++
@@ -1,7 +0,0 @@
-#include "usecases.h"
-
-int main(int argc, char **argv)
-{
-	fxr_client_main("test-shm-loc", "filesource", "filedest");
-	return 0;
-}
--- src/c/libcielshm/test/serv.c
+++
@@ -1,7 +0,0 @@
-#include "usecases.h"
-
-int main(int argc, char **argv)
-{
-	fxr_server_main("test-shm-loc");
-	return 0;
-}
